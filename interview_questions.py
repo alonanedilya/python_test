d4ed1e7efebcf7eb4cdbbfa1f7df056fe10760c0
alonanedilya <alonanedilya@gmail.com>
2025-03-01 11:57:37 -0500

    apple commit

---
+++ interview_questions.py
@@ -0,0 +1,10 @@
+# Define the original string
+original_string = "hello"
+
+# Reverse the original string using the reversed() function
+# and join the characters back together into a new string
+
+reversed_string = ''.join(reversed(original_string))
+
+# Print the reversed string
+print(reversed_string)
